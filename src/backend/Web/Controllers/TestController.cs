757e9f2c98c97f529f400e4fc3a4de41c9ddb223
Mir Anupam Hossain Akib <mirakib25@gmail.com>
2025-02-04 00:32:56 +0600

    clean architecture basic structure implemented

---
+++ src/backend/Web/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApp.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult AddTwoNumbers(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return Ok($"The sum is {sum}");
+        }
+    }
+}
